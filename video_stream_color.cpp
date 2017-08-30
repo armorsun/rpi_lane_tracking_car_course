f54518ee1af99f659b33edcbd3a35b4fafe57892
Sunny <gsunny4178@gmail.com>
2017-08-30 09:56:07 +0800

    big fix

--- video_stream_color.cpp
+++ video_stream_color.cpp
@@ -4,19 +4,19 @@ using namespace cv;
 using namespace std;
 
 int main() {
-VideoCapture stream(0); //0 is the id of the camera
-//you can find the id by type “ll /dev/video*” in a terminal
-if (!stream.isOpened()) cout << "cannot open camera”;
-//check if video device has been initialized
+	VideoCapture stream(0); //0 is the id of the camera
+	//you can find the id by type “ll /dev/video*” in a terminal
+	if (!stream.isOpened()) cout << "cannot open camera”;
+	//check if video device has been initialized
 
- stream.set(CV_CAP_PROP_BUFFERSIZE, 3);
- char checkForEscKey = 0;
- Mat cameraFrame; //store the frame data
- while (charCheckForEscKey != 27 && stream.isOpened()) {
- stream.read(cameraFrame);
- namedWindow("imgOriginal", CV_WINDOW_NORMAL);
- imshow("imgOriginal", cameraFrame);
- charCheckForEscKey = waitKey(1);
- }
- return 0;
+	stream.set(CV_CAP_PROP_BUFFERSIZE, 3);
+	char checkForEscKey = 0;
+	Mat cameraFrame; //store the frame data
+	while (checkForEscKey != 27 && stream.isOpened()) {
+	stream.read(cameraFrame);
+	namedWindow("imgOriginal", CV_WINDOW_NORMAL);
+	imshow("imgOriginal", cameraFrame);
+	checkForEscKey = waitKey(1);
+ 	}
+ 	return 0;
 }
--- video_stream_gray.cpp
+++ video_stream_gray.cpp
@@ -4,23 +4,23 @@ using namespace cv;
 using namespace std;
 
 int main() {
-VideoCapture stream(0);   //0 is the id of the camera
-//you can find the id by type “ll /dev/video*” in a terminal
-if (!stream.isOpened()){
-cout << "cannot open camera";
-return 0;
-}
-//check if video device has been initialized
+	VideoCapture stream(0);   //0 is the id of the camera
+	//you can find the id by type “ll /dev/video*” in a terminal
+	if (!stream.isOpened()){
+	cout << "cannot open camera";
+	return 0;
+	}
+	//check if video device has been initialized
 
-char checkForEscKey = 0;
-Mat cameraFrame_color, cameraFrame_gray; //store the frame data
+	char checkForEscKey = 0;
+	Mat cameraFrame_color, cameraFrame_gray; //store the frame data
 
-while (checkForEscKey != 27 && stream.isOpened()) {
-stream.read(cameraFrame_color);
-cvtColor(cameraFrame_color, cameraFrame_gray, COLOR_BGR2GRAY);
-namedWindow("imgOriginal", CV_WINDOW_NORMAL);
-imshow("imgOriginal", cameraFrame_gray);
-checkForEscKey = waitKey(1);
-}
-return 0;
+	while (checkForEscKey != 27 && stream.isOpened()) {
+	stream.read(cameraFrame_color);
+	cvtColor(cameraFrame_color, cameraFrame_gray, COLOR_BGR2GRAY);
+	namedWindow("imgOriginal", CV_WINDOW_NORMAL);
+	imshow("imgOriginal", cameraFrame_gray);
+	checkForEscKey = waitKey(1);
+	}
+	return 0;
 }
